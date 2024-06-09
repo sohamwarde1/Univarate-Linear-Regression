4d9b7a2a6dd821aea911bdee346d3acf5b47aea2
sohamwarde1 <sohamwarde1@gmail.com>
2024-06-09 16:37:48 +0530

    Add gradient descent and plot line

--- LinearReg.py
+++ LinearReg.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_train = np.array([15,14,13,12,11,10])
-y_train = np.array([741,583,446,435,359,330])
+x_train = np.array([1.0, 2.0, 3.0, 5.0])
+y_train = np.array([300.0, 500.0,650.0, 1000])
 
 
 def costf(x_train,y_train,w,b):
@@ -10,7 +10,7 @@ def costf(x_train,y_train,w,b):
 	err = 0
 	m  = len(x_train)
 
-	for i in range(n):
+	for i in range(m):
 		f = (w*x_train[i])+b
 		err = err + ((f-y_train[i])**2)
 
@@ -23,7 +23,7 @@ def compute_gradient(x_train,y_train,w,b):
 	 
 	 wv = 0
 	 bv = 0
-	 m = x.shape[0]
+	 m = x_train.shape[0]
 
 	 for i in range(m):
 	 	f = (w*x_train[i])+b
@@ -36,35 +36,50 @@ def compute_gradient(x_train,y_train,w,b):
 	 return wv/m , bv/m
 
 
-def gradientdescent(wi, bi, x_train, y_train, alpha):
+def gradientdescent(wi, bi, x_train, y_train, alpha, iters):
 
 	minc = costf(x_train,y_train,wi,bi)
 	wlist = [wi]
 	blist = [bi]
 
-	while True:
-		wn1,bn1 = compute_gradient(x_train, y_train, wlist[-1], blistp-1)
+	for i in range(iters):
+		wn1,bn1 = compute_gradient(x_train, y_train, wlist[-1], blist[-1])
 
 		wnew = wlist[-1] - (alpha*wn1)
 		bnew = blist[-1] - (alpha*bn1)
+		print(wnew)
 
-		cost = costf(x_train,y_train,w,b)
+		cost = costf(x_train,y_train,wnew,bnew)
 
-		if cost<=minc:
-			minc = cost
-		else:
-			return wlist[-1],blist[-1]
 
 		wlist.append(wnew)
 		blist.append(bnew)
-
+	return wlist[-1],blist[-1]
 		
-def plotline(w,b,x_train):
+def createline(x_train,y_train, alpha,iters):
 	wi = 0
 	bi = 0
+
+	m = x_train.shape[0]
 	
+	w,b = gradientdescent(wi,bi,x_train,y_train,alpha,iters)
+
+	f = []
+
+	for i in range(m):
+		f.append((w*x_train[i])+b)
+
+	return f
+
+
 
 
 
 plt.scatter(x_train,y_train,c = 'r')
+alpha = 0.1
+iters = 10
+f = createline(x_train, y_train, alpha,iters)
+
+plt.plot(x_train, f, c = 'b')
+
 plt.show()
